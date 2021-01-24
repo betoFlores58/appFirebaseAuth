94707ac86b9a7a68c1c7a4dd8d8a34887fa7ec04
betoFlores58 <albertfr@live.com.mx>
2021-01-24 00:40:33 -0600

    Initial Commit

---
+++ appFirebaseFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appFirebaseFinal
+//
+//  Created by Alberto Flores on 24/01/21.
+//  Copyright © 2021 Alberto Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
